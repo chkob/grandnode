f052bee6b0bc01a9279ae8486eda33f8584322a4
Chatchai Daecha <chkob81@hotmail.com>
2019-06-23 13:27:23 +0700

    modify to support listening for configurable port
    # Conflicts:
    #	Grand.Web/App_Data/appsettings.json

--- Grand.Web/Program.cs
+++ Grand.Web/Program.cs
@@ -1,18 +1,42 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net;
 
 namespace Grand.Web
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+   public class Program
+   {
+      public static IHostingEnvironment HostingEnvironment { get; set; }
+      public static void Main(string[] args)
+      {
+         try
+         {
             CreateWebHostBuilder(args).Build().Run();
-        }
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .CaptureStartupErrors(true)
-                .UseSetting(WebHostDefaults.PreventHostingStartupKey, "true")
-                .UseStartup<Startup>();
-    }
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine($"{ex.Message}");
+         }
+      }
+      public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+      {
+         var curDir = Directory.GetCurrentDirectory();
+         var host = WebHost.CreateDefaultBuilder(args)
+               .CaptureStartupErrors(true)
+               .UseSetting(WebHostDefaults.PreventHostingStartupKey, "true")
+               .UseStartup<Startup>()
+               .ConfigureKestrel((context, options) =>
+               {
+                  var httpPort = context.Configuration.GetValue<int>("Hosting:ListeningPorts:Http");
+                  var httpsPort = context.Configuration.GetValue<int>("Hosting:ListeningPorts:Https");
+
+                  options.Listen(IPAddress.Any, httpPort);
+               })
+              ;
+         return host;
+      }
+   }
 }
